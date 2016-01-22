efc3efc9e01c0f41694e66dac5419bdc1f0fa9a4
quzhaomei <1528783208@qq.com>
2016-01-22 22:49:04 +0800

    ..

---
+++ src/com/rycf/gjb/dto/ThirdGuideHistoryDTO.java
@@ -0,0 +1,67 @@
+package com.rycf.gjb.dto;
+
+import java.util.Date;
+
+/**
+ * 第三方导购客户信息
+ * @author Administrator
+ */
+public class ThirdGuideHistoryDTO {
+	private Integer getMoreId;
+	private Integer guideId;
+	private String nickname;
+	private String headimgurl;
+	
+	private Date lastTime;
+	
+	private int isFriend;
+	
+	private Integer uncheckCount;
+	
+	public Date getLastTime() {
+		return lastTime;
+	}
+	public void setLastTime(Date lastTime) {
+		this.lastTime = lastTime;
+	}
+	public int getIsFriend() {
+		return isFriend;
+	}
+	public void setIsFriend(int isFriend) {
+		this.isFriend = isFriend;
+	}
+	
+	public Integer getGuideId() {
+		return guideId;
+	}
+	public void setGuideId(Integer guideId) {
+		this.guideId = guideId;
+	}
+	public Integer getGetMoreId() {
+		return getMoreId;
+	}
+	public void setGetMoreId(Integer getMoreId) {
+		this.getMoreId = getMoreId;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getHeadimgurl() {
+		return headimgurl;
+	}
+	public void setHeadimgurl(String headimgurl) {
+		this.headimgurl = headimgurl;
+	}
+	
+	public Integer getUncheckCount() {
+		return uncheckCount;
+	}
+	public void setUncheckCount(Integer uncheckCount) {
+		this.uncheckCount = uncheckCount;
+	}
+	
+	
+}
---
+++ src/com/rycf/gjb/entity/UserToGuide.java
@@ -0,0 +1,26 @@
+package com.rycf.gjb.entity;
+
+public class UserToGuide {
+	private Integer id;
+	private Integer getMoreId;
+	private Integer guideId;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getGetMoreId() {
+		return getMoreId;
+	}
+	public void setGetMoreId(Integer getMoreId) {
+		this.getMoreId = getMoreId;
+	}
+	public Integer getGuideId() {
+		return guideId;
+	}
+	public void setGuideId(Integer guideId) {
+		this.guideId = guideId;
+	}
+	
+}
